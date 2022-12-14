a7abff4745a1b1fcbf69e42701a3f4814c73e761
Vanessa <tauchertvanessa@gmail.com>
2022-12-14 00:08:41 -0300

    first commit

---
+++ src/main/java/br/com/van/javaspring/JavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.van.javaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringApplication.class, args);
+	}
+
+}
