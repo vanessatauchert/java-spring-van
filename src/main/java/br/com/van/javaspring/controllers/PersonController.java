886495899b1a95fc5a04afe2709341d20922b7f9
Vanessa <tauchertvanessa@gmail.com>
2022-12-14 21:18:10 -0300

    new branch person

--- src/main/java/br/com/van/javaspring/controllers/PersonController.java
+++ src/main/java/br/com/van/javaspring/controllers/PersonController.java
@@ -3,9 +3,8 @@ package br.com.van.javaspring.controllers;
 import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicLong;
 
-
-
 @RestController
+
 public class PersonController {
     private static final AtomicLong counter  = new AtomicLong();
 
